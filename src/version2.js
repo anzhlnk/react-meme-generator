10f1d9dbc8c4fda1e95c13814b479051a178607d
Hanna <anzhelenyuk@gmail.com>
2022-05-12 14:27:11 +0200

    Generate customer meme

--- src/version2.js
+++ src/version2.js
@@ -1,39 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 export default function MemeGenerator() {
   const [text, setText] = useState({
     topText: '',
     bottomText: '',
   });
-
-  const [allData, setAllData] = useState([]);
-  const arrayOfLinks = [];
   const [customerTemplate, setCustomerTemplate] = useState('');
-
-  //Version1
-  useEffect(() => {
-    fetch('https://api.memegen.link/templates')
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        setAllData(response);
-      })
-      .catch(() => {
-        return 'Error';
-      });
-  }, []);
-
-  for (let i = 0; i < allData.length; i++) {
-    arrayOfLinks.push(allData[i].blank);
-  }
-
   const [image, setImage] = useState(
     'https://api.memegen.link/images/grumpycat.png',
   );
-
-  console.log(image);
 
   return (
     <form
@@ -45,7 +21,6 @@ export default function MemeGenerator() {
       <label>
         Top text{' '}
         <input
-          type="text"
           name="topText"
           placeholder="Add Top Text"
           onChange={(event) => {
@@ -62,7 +37,6 @@ export default function MemeGenerator() {
       <label>
         Bottom text
         <input
-          type="text"
           name="bottomText"
           placeholder="Add Bottom Text"
           onChange={(event) => {
@@ -79,7 +53,6 @@ export default function MemeGenerator() {
       <label>
         Meme template
         <input
-          type="text"
           name="image"
           placeholder="Add template"
           onChange={(event) => {
@@ -87,19 +60,7 @@ export default function MemeGenerator() {
           }}
         />
       </label>
-      <button
-        onClick={() => {
-          customerTemplate === ''
-            ? setImage(
-                arrayOfLinks[Math.floor(Math.random() * arrayOfLinks.length)],
-              )
-            : setImage(
-                `https://api.memegen.link/images/${customerTemplate}.png`,
-              );
-        }}
-      >
-        Generate
-      </button>
+      <button>Generate</button>
       <button>Download</button>
       <br />
       <img src={image} alt="meme" />
